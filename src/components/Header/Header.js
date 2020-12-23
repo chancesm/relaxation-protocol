3bfcd68474bf1d2c489a8379fbff3a131628b4f4
Chance Murray <chance.s.murray@gmail.com>
2020-12-23 23:51:53 +0000

    hide select with no phases

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,9 @@ function Header(props) {
     <div className="Header">
       <div className="title">{props.title}</div>
       <div className="right">
-        <select value={props.selectedPhase} onChange={(e) => props.handler(e.target.value)}>
+        {props.options.length > 0 ? (<select value={props.selectedPhase} onChange={(e) => props.handler(e.target.value)}>
           {props.options.map((o, i) => <option key={o.id} value={o.fields.Name}>{o.fields.Name}</option>)}
-        </select>
+        </select>) : null }
       </div>
     </div>
   );
